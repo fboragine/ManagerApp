6b1b03ddf3148cefaf6e50ab867d5bf3efaa3227
TheFisher10 <f.boragine@studenti.uniba.it>
2021-02-16 16:11:47 +0100

    initial commit

---
+++ app/src/main/java/it/uniba/di/sms2021/managerapp/MainActivity.java
@@ -0,0 +1,14 @@
+package it.uniba.di.sms2021.managerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
