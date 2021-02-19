ab7959419053f73276ca927462620ba1607b5336
Leomrtn7 <leomrtn997@gmail.com>
2021-02-19 17:48:19 +0100

    Inizializzazzione Classi CorsoDiStudio, Docente.

--- app/src/main/java/entities/CorsoDiStudio.java
+++ app/src/main/java/entities/CorsoDiStudio.java
@@ -1,4 +1,38 @@
 package entities;
 
 public class CorsoDiStudio {
+
+    private String idCorsoDiStudio;
+    private String nome;
+    private String descrizione;
+
+    public CorsoDiStudio(String idCorsoDiStudio, String nome, String descrizione) {
+        this.idCorsoDiStudio = idCorsoDiStudio;
+        this.nome = nome;
+        this.descrizione = descrizione;
+    }
+
+    public String getIdCorsoDiStudio() {
+        return idCorsoDiStudio;
+    }
+
+    public void setIdCorsoDiStudio(String idCorsoDiStudio) {
+        this.idCorsoDiStudio = idCorsoDiStudio;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
 }
--- app/src/main/java/entities/Docente.java
+++ app/src/main/java/entities/Docente.java
@@ -1,4 +1,60 @@
 package entities;
 
+import java.util.Date;
+
 public class Docente {
+
+    private String matricola;
+    private String nome;
+    private String cognome;
+    private Date data;
+    private String percorsoImmagine;
+
+    public Docente(String matricola, String nome, String cognome, Date data, String percorsoImmagine) {
+        this.matricola = matricola;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.data = data;
+        this.percorsoImmagine = percorsoImmagine;
+    }
+
+    public String getMatricola() {
+        return matricola;
+    }
+
+    public void setMatricola(String matricola) {
+        this.matricola = matricola;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getPercorsoImmagine() {
+        return percorsoImmagine;
+    }
+
+    public void setPercorsoImmagine(String percorsoImmagine) {
+        this.percorsoImmagine = percorsoImmagine;
+    }
 }
