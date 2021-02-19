5f32f62665b8ed27400d7612035604501e086a45
TheFisher10 <f.boragine@studenti.uniba.it>
2021-02-19 18:13:47 +0100

    Creazione toolbar superiore

--- app/src/main/java/it/uniba/di/sms2021/managerapp/Home.java
+++ app/src/main/java/it/uniba/di/sms2021/managerapp/Home.java
@@ -1,14 +1,50 @@
 package it.uniba.di.sms2021.managerapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Home extends AppCompatActivity {
+
+    private static final String TAG = "SimpleToolbarTest";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.top_toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        toolbar.setTitle("Home");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Log.d(TAG, "Settings");
+            return true;
+        }
+
+        if (id == R.id.action_filter) {
+            Log.d(TAG, "Filter");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
