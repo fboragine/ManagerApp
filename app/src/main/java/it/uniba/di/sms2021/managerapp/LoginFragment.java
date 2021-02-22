fb810fcba5bce2f22db95301ba7431251dc04552
Nico3834 <n.cantatore3@studenti.uniba.it>
2021-02-22 16:03:04 +0100

    Rinomina pagina login.

--- app/src/main/java/it/uniba/di/sms2021/managerapp/LoginFragment.java
+++ app/src/main/java/it/uniba/di/sms2021/managerapp/LoginFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Login2Fragment#newInstance} factory method to
+ * Use the {@link LoginFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Login2Fragment extends Fragment {
+public class LoginFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class Login2Fragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Login2Fragment() {
+    public LoginFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class Login2Fragment extends Fragment {
      * @return A new instance of fragment Login2Fragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static Login2Fragment newInstance(String param1, String param2) {
-        Login2Fragment fragment = new Login2Fragment();
+    public static LoginFragment newInstance(String param1, String param2) {
+        LoginFragment fragment = new LoginFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class Login2Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login2, container, false);
+        return inflater.inflate(R.layout.fragment_login, container, false);
     }
 }
