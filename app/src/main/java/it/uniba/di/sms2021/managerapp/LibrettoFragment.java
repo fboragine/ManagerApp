90a3dcfb6d68ad5905ea43b9f265042ee0b4b40e
TheFisher10 <f.boragine@studenti.uniba.it>
2021-02-25 17:06:01 +0100

    Aggiunta StudentActivity e modificato l'uso delle navigations. Aggiunti pulsanti alle top_toolbar delle schermate ProfileFragment e EditProfileFragment

--- app/src/main/java/it/uniba/di/sms2021/managerapp/LibrettoFragment.java
+++ app/src/main/java/it/uniba/di/sms2021/managerapp/LibrettoFragment.java
@@ -107,17 +107,14 @@ public class LibrettoFragment extends Fragment {
                     Menu.NONE,
                     searchId,
                     1,
-                    "Search"
+                    getString(R.string.search)
             );
 
             // Set an icon for the new menu item
             search.setIcon(R.drawable.ic_search);
 
             // Set the show as action flags for new menu item
-            search.setShowAsActionFlags(
-                    MenuItem.SHOW_AS_ACTION_WITH_TEXT |
-                            MenuItem.SHOW_AS_ACTION_ALWAYS
-            );
+            search.setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_IF_ROOM);
 
             // Set a click listener for the new menu item
             search.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
@@ -138,17 +135,14 @@ public class LibrettoFragment extends Fragment {
                     Menu.NONE,
                     filterId,
                     2,
-                    "Filter"
+                    getString(R.string.action_filter)
             );
 
             // Set an icon for the new menu item
             filter.setIcon(R.drawable.ic_baseline_filter_alt_24);
 
             // Set the show as action flags for new menu item
-            filter.setShowAsActionFlags(
-                    MenuItem.SHOW_AS_ACTION_WITH_TEXT |
-                            MenuItem.SHOW_AS_ACTION_ALWAYS
-            );
+            filter.setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_IF_ROOM);
 
             // Set a click listener for the new menu item
             filter.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
--- app/src/main/java/it/uniba/di/sms2021/managerapp/ProfileFragment.java
+++ app/src/main/java/it/uniba/di/sms2021/managerapp/ProfileFragment.java
@@ -9,9 +9,13 @@ import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -70,6 +74,8 @@ public class ProfileFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setHasOptionsMenu(true);
+
         Log.d(TAG, "Profilo");
 
         Intent intent = getActivity().getIntent();
@@ -109,11 +115,54 @@ public class ProfileFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
-    /*@Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        if (context instanceof Callback) {
-            callback = (GuestActivity) context;
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.toolbar_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Toast.makeText(getActivity().getApplicationContext(), item.getTitle()+" Clicked", Toast.LENGTH_SHORT).show();
+            return true;
         }
-    }*/
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(@NonNull Menu menu) {
+
+        // Add Edit Menu Item
+        int editId = StudentActivity.EDIT_ITEM_ID;
+        if (menu.findItem(editId) == null) {
+            // If it not exists then add the menu item to menu
+            MenuItem edit = menu.add(
+                    Menu.NONE,
+                    editId,
+                    1,
+                    getString(R.string.edit)
+            );
+
+            // Set an icon for the new menu item
+            edit.setIcon(R.drawable.ic_edit);
+
+            // Set the show as action flags for new menu item
+            edit.setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+
+            // Set a click listener for the new menu item
+            edit.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    Toast.makeText(getActivity().getApplicationContext(), item.getTitle()+" Clicked", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+            });
+
+        }
+
+        super.onPrepareOptionsMenu(menu);
+    }
 }
