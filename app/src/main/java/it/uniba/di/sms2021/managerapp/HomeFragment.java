8f8eef878cc7a399a1a5f763a39f41f07b354c3d
TheFisher10 <f.boragine@studenti.uniba.it>
2021-02-27 18:11:39 +0100

    Creato tabsLayout nella home

--- app/src/main/java/it/uniba/di/sms2021/managerapp/HomeFragment.java
+++ app/src/main/java/it/uniba/di/sms2021/managerapp/HomeFragment.java
@@ -7,6 +7,11 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,9 +20,13 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import javax.security.auth.callback.Callback;
 
@@ -32,10 +41,16 @@ public class HomeFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "SimpleToolbarTest";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
+    private FragmentStateAdapter pagerAdapter;
+
+    private View viewHome;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -62,19 +77,91 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //getActivity().setContentView(R.layout.activity_student);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
 
         setHasOptionsMenu(true);
+
+        /*
+        // Create a new Tab named "In Progress"
+        TabLayout.Tab inProgressTab = tabLayout.newTab();
+        inProgressTab.setText(getString(R.string.in_progress)); // set the Text for the In Progress Tab
+        tabLayout.addTab(inProgressTab); // add  the tab at in the TabLayout
+
+        // Create a new Tab named "Closed"
+        TabLayout.Tab closedTab = tabLayout.newTab();
+        closedTab.setText(getString(R.string.closed)); // set the Text for the In Progress Tab
+        tabLayout.addTab(closedTab); // add  the tab at in the TabLayout
+
+         */
+
+        /*
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // get the current selected tab's position and replace the fragment accordingly
+                Fragment fragment = null;
+                switch (tab.getPosition()) {
+                    case 0:
+                        fragment = new InProgressProjectsFragment();
+                        break;
+                    case 1:
+                        fragment = new ClosedProjectsFragment();
+                        break;
+                }
+                FragmentManager fm = getActivity().getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.home_frame_layout, fragment);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                ft.commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+         */
+    }
+
+    private MyAdapter createAdapter() {
+        return new MyAdapter(getActivity());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        viewHome = inflater.inflate(R.layout.fragment_home, container, false);
+
+        tabLayout = (TabLayout) viewHome.findViewById(R.id.tab_home);
+        viewPager = (ViewPager2) viewHome.findViewById(R.id.home_view_pager);
+        viewPager.setAdapter(createAdapter());
+
+        if(tabLayout == null) {
+            Log.d(TAG, "tabLayout è null");
+        }
+        if(viewPager == null) {
+            Log.d(TAG, "viewpager è null");
+        }
+
+        new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText("Tab " + (position + 1));
+            }
+        }).attach();
+
+        return viewHome;
     }
 
     @Override
