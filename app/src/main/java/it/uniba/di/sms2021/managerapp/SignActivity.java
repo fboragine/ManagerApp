cb76e2b04fb53e73580ba79251577d7d1e20f24d
TheFisher10 <f.boragine@studenti.uniba.it>
2021-03-01 12:34:56 +0100

    Aggiunta toolbar inferiore nella SignActivity per annullare o confermare la registrazione. Rimossi i due button che svolgevano tale compito

--- app/src/main/java/it/uniba/di/sms2021/managerapp/SignActivity.java
+++ app/src/main/java/it/uniba/di/sms2021/managerapp/SignActivity.java
@@ -3,11 +3,14 @@ package it.uniba.di.sms2021.managerapp;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.AttributeSet;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,12 +40,35 @@ public class SignActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.bottom_register_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
     @Nullable
     @Override
     public View onCreateView(@Nullable View parent, @NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
         return super.onCreateView(parent, name, context, attrs);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_register_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.do_registration:
+                register();
+                break;
+            case R.id.cancel_registration:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -65,6 +91,51 @@ public class SignActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void register(View v) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        EditText mat = (EditText) findViewById(R.id.serial_number);
+        EditText nome = (EditText) findViewById(R.id.name);
+        EditText cognome = (EditText) findViewById(R.id.surname);
+        EditText email = (EditText) findViewById(R.id.email);
+        EditText cDs = (EditText) findViewById(R.id.course);
+
+        EditText pw = (EditText) findViewById(R.id.password);
+
+
+
+        Studente aux = new Studente(mat.getText().toString(),
+                nome.getText().toString(),
+                cognome.getText().toString(),
+                email.getText().toString(),
+                cDs.getText().toString());
+
+        Map<String ,String> user = new HashMap<>();
+        user.put("nome",aux.getNome());
+        user.put("cognome",aux.getCognome());
+        user.put("email",aux.getEmail());
+        user.put("matricola",aux.getMatricola());
+        user.put("cDs",aux.getcDs());
+        user.put("percorsoImg", "");
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.createUserWithEmailAndPassword(aux.getEmail(), pw.getText().toString());
+
+        user.put("id", mAuth.getUid());
+
+        //Getting Reference to "users" collection
+        //REMEMBER: at this point, no collection will be created
+        CollectionReference collectionReference = db.collection("studenti");
+
+        //Getting reference to "new doc" document
+        //REMEMBER: at this point, no document will be created either
+
+        DocumentReference documentReference = collectionReference.document(mAuth.getUid());
+        documentReference.set(user);
+    }
+
+    private void register() {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         EditText mat = (EditText) findViewById(R.id.serial_number);
