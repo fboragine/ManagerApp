efaedfb2c81e47cdcb87def943d4083d0c9c8987
TheFisher10 <f.boragine@studenti.uniba.it>
2021-03-03 17:56:52 +0100

    Corretto layout card per scorrimento orizzontale nella HomeFragment. Aggiunti progetti chiusi e rimossi i tab. Cambiati i colori dell'applicazione

--- app/src/main/java/it/uniba/di/sms2021/managerapp/HomeFragment.java
+++ app/src/main/java/it/uniba/di/sms2021/managerapp/HomeFragment.java
@@ -9,6 +9,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
@@ -28,27 +30,25 @@ import android.widget.Toolbar;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import javax.security.auth.callback.Callback;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
     private static final String TAG = "SimpleToolbarTest";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    private TabLayout tabLayout;
-    private ViewPager2 viewPager;
-    private FragmentStateAdapter pagerAdapter;
+    private ArrayList<String> inProgressProjectName;
+    private ArrayList<String> inProgressProjectExam;
+    private ArrayList<String> closedProjectName;
+    private ArrayList<String> closedProjectExam;
+    private RecyclerView inProgressRecyclerView;
+    private RecyclerView closedRecyclerView;
+    private RecyclerView.LayoutManager inProgressLayoutManager;
+    private RecyclerView.LayoutManager closedLayoutManager;
+    private RecyclerView.Adapter inProgressAdapter;
+    private RecyclerView.Adapter closedAdapter;
 
     private View viewHome;
 
@@ -56,85 +56,11 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //getActivity().setContentView(R.layout.activity_student);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
 
         setHasOptionsMenu(true);
-
-        /*
-        // Create a new Tab named "In Progress"
-        TabLayout.Tab inProgressTab = tabLayout.newTab();
-        inProgressTab.setText(getString(R.string.in_progress)); // set the Text for the In Progress Tab
-        tabLayout.addTab(inProgressTab); // add  the tab at in the TabLayout
-
-        // Create a new Tab named "Closed"
-        TabLayout.Tab closedTab = tabLayout.newTab();
-        closedTab.setText(getString(R.string.closed)); // set the Text for the In Progress Tab
-        tabLayout.addTab(closedTab); // add  the tab at in the TabLayout
-
-         */
-
-        /*
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                // get the current selected tab's position and replace the fragment accordingly
-                Fragment fragment = null;
-                switch (tab.getPosition()) {
-                    case 0:
-                        fragment = new InProgressProjectsFragment();
-                        break;
-                    case 1:
-                        fragment = new ClosedProjectsFragment();
-                        break;
-                }
-                FragmentManager fm = getActivity().getSupportFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.replace(R.id.home_frame_layout, fragment);
-                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-                ft.commit();
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-         */
-    }
-
-    private MyAdapter createAdapter() {
-        return new MyAdapter(getActivity());
     }
 
     @Override
@@ -143,23 +69,25 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         viewHome = inflater.inflate(R.layout.fragment_home, container, false);
 
-        tabLayout = (TabLayout) viewHome.findViewById(R.id.tab_home);
-        viewPager = (ViewPager2) viewHome.findViewById(R.id.home_view_pager);
-        viewPager.setAdapter(createAdapter());
+        inProgressProjectName = new ArrayList<>(Arrays.asList("Project 1", "Project 2", "Project 3", "Project 4", "Project 5", "Project 6", "Project 7", "Project 8", "Project 9", "Project 10"));
+        inProgressProjectExam = new ArrayList<>(Arrays.asList("Exam 1", "Exam 2", "Exam 3", "Exam 4", "Exam 5", "Exam 6", "Exam 7", "Exam 8", "Exam 9", "Exam 10"));
 
-        if(tabLayout == null) {
-            Log.d(TAG, "tabLayout è null");
-        }
-        if(viewPager == null) {
-            Log.d(TAG, "viewpager è null");
-        }
+        closedProjectName = new ArrayList<>(Arrays.asList("Closed Project 1", "Closed Project 2", "Closed Project 3", "Closed Project 4", "Closed Project 5", "Closed Project 6", "Closed Project 7", "Closed Project 8", "Closed Project 9", "Closed Project 10"));
+        closedProjectExam = new ArrayList<>(Arrays.asList("Exam 1", "Exam 2", "Exam 3", "Exam 4", "Exam 5", "Exam 6", "Exam 7", "Exam 8", "Exam 9", "Exam 10"));
 
-        new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
-            @Override
-            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-                tab.setText("Tab " + (position + 1));
-            }
-        }).attach();
+        inProgressRecyclerView = viewHome.findViewById(R.id.inProgressRecyclerView);
+        inProgressRecyclerView.setHasFixedSize(true);
+        closedRecyclerView = viewHome.findViewById(R.id.closedRecyclerView);
+        closedRecyclerView.setHasFixedSize(true);
+
+        inProgressLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
+        inProgressRecyclerView.setLayoutManager(inProgressLayoutManager);
+        closedLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
+        closedRecyclerView.setLayoutManager(closedLayoutManager);
+        inProgressAdapter = new RecyclerViewAdapter(getActivity().getApplicationContext(), inProgressProjectName, inProgressProjectExam);
+        inProgressRecyclerView.setAdapter(inProgressAdapter);
+        closedAdapter = new RecyclerViewAdapter(getActivity().getApplicationContext(), closedProjectName, closedProjectExam);
+        closedRecyclerView.setAdapter(closedAdapter);
 
         return viewHome;
     }
@@ -237,7 +165,6 @@ public class HomeFragment extends Fragment {
                     return true;
                 }
             });
-
         }
 
         super.onPrepareOptionsMenu(menu);
