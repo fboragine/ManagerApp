f648e728f3b005ad9f124ed901607d7fae623fea
TheFisher10 <f.boragine@studenti.uniba.it>
2021-03-06 11:35:53 +0100

    Rimosse istruzioni inutili file ProfileFragment.java Aggiunte variabili string

--- app/src/main/java/it/uniba/di/sms2021/managerapp/ProfileFragment.java
+++ app/src/main/java/it/uniba/di/sms2021/managerapp/ProfileFragment.java
@@ -29,21 +29,8 @@ import javax.security.auth.callback.Callback;
 
 import entities.Studente;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private GuestActivity callback;
     private FirebaseDatabase db;
     private DatabaseReference userRef;
@@ -52,24 +39,6 @@ public class ProfileFragment extends Fragment {
 
     public ProfileFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
