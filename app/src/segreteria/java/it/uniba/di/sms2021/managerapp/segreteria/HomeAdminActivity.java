d1bca8a8ef365a07f9e7ecfe4058551986cf27fd
TheFisher10 <f.boragine@studenti.uniba.it>
2021-03-13 10:12:11 +0100

    Creazione Home Admin

---
+++ app/src/segreteria/java/it/uniba/di/sms2021/managerapp/segreteria/HomeAdminActivity.java
@@ -0,0 +1,16 @@
+package it.uniba.di.sms2021.managerapp.segreteria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import it.uniba.di.sms2021.managerapp.R;
+
+public class HomeAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_admin);
+    }
+}
