f27e8adc16d27f454fb568de05c9b1384ee88507
Nic-Sanc <nicolosancilio@hotmail.it>
2021-05-11 23:42:50 +0200

    Visualizzazione dei progetti di cui il docente insegna nel corrispondente corso di studio

--- app/src/main/java/it/uniba/di/sms2021/managerapp/loggedUser/HomeFragment.java
+++ app/src/main/java/it/uniba/di/sms2021/managerapp/loggedUser/HomeFragment.java
@@ -25,16 +25,17 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Objects;
 
 import it.uniba.di.sms2021.managerapp.R;
+import it.uniba.di.sms2021.managerapp.entities.Esame;
 import it.uniba.di.sms2021.managerapp.project.AddNewProjectActivity;
 import it.uniba.di.sms2021.managerapp.service.RecyclerViewAdapter;
 import it.uniba.di.sms2021.managerapp.entities.Progetto;
 
 public class HomeFragment extends Fragment implements View.OnClickListener{
-
-    private static final String TAG = "SimpleToolbarTest";
 
     private ArrayList<Progetto> inProgressProject;
     private ArrayList<Progetto> closedProject;
@@ -48,13 +49,21 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
     private Button addProjectBtn;
 
     private FirebaseAuth mAuth;
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseFirestore db;
 
     private View viewHome;
+    private File loggedStudente;
+    private File loggedDocente;
+    private ArrayList<Esame> examList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        examList = new ArrayList<>();
 
         setHasOptionsMenu(true);
     }
@@ -68,75 +77,176 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
         inProgressProject = new ArrayList<>();
         closedProject = new ArrayList<>();
 
-        addProjectBtn = (Button) viewHome.findViewById(R.id.add_project_btn);
-        addProjectBtn.setOnClickListener(this);
+        addProjectBtn = viewHome.findViewById(R.id.add_project_btn);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("progetti").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        String pathStudente = getActivity().getExternalFilesDir(null).getPath() + "/studenti.srl";
+        String pathDocente = getActivity().getExternalFilesDir(null).getPath() + "/docenti.srl";
+        loggedStudente = new File(pathStudente);
+        loggedDocente = new File(pathDocente);
+
+        if (loggedStudente.exists()) {
+            addProjectBtn.setOnClickListener(this);
+        }else if(loggedDocente.exists()) {
+            addProjectBtn.setVisibility(View.GONE);
+        }
+
+        createProjectView();
+
+        return viewHome;
+    }
+
+    private void createProjectView() {
+
+        if (loggedStudente.exists()) {
+
+            db.collection("progetti").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Progetto progetto = new Progetto(document.getString("id"),
+                                    document.getString("nome"),
+                                    document.getString("descrizione"),
+                                    document.getString("codiceEsame"),
+                                    document.getString("dataCreazione"),
+                                    (ArrayList<String>) document.get("idStudenti"),
+                                    document.getBoolean("stato"));
+
+                            visualizzaProgettiStudente(progetto);
+                        }
+                        initProjectView();
+                    }
+                }
+            });
+
+        }
+        else if (loggedDocente.exists()) {
+
+            db.collection("esami").whereArrayContains("idDocenti", mAuth.getCurrentUser().getUid()).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+
+                            Esame esame = new Esame(document.getString("id"),
+                                    document.getString("nome"),
+                                    document.getString("commento"),
+                                    document.getString("descrizione"),
+                                    document.getString("cDs"),
+                                    (ArrayList<String>) document.get("idDocenti"));
+                            getDocentProject(esame);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    private void getDocentProject(Esame esame) {
+        db.collection("progetti").whereEqualTo("codiceEsame", esame.getId()).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if(task.isSuccessful()) {
-                    mAuth = FirebaseAuth.getInstance();
+                if (task.isSuccessful()) {
                     for (QueryDocumentSnapshot document : task.getResult()) {
-                        Progetto progetto = new Progetto(document.getString("id"), document.getString("nome"), document.getString("descrizione"),
-                                document.getString("codiceEsame"), document.getString("dataCreazione"),
-                                (ArrayList<String>) document.get("idStudenti"), document.getBoolean("stato"));
 
-                        // sezione di assegnamento dei progetti chiusi
-                        if(progetto.isClose()) {
-                            ArrayList<String> idStudenti = progetto.getIdStudenti();
-                            int count = 0;
-                            boolean flag = false;
+                        Progetto progetto = new Progetto(document.getString("id"),
+                                document.getString("nome"),
+                                document.getString("descrizione"),
+                                document.getString("codiceEsame"),
+                                document.getString("dataCreazione"),
+                                (ArrayList<String>) document.get("idStudenti"),
+                                document.getBoolean("stato"));
 
-                            do {
-                                if (idStudenti.get(count).equals(mAuth.getCurrentUser().getUid())) {
-                                    closedProject.add(progetto);
-                                    flag = true;
-                                }
-                                count++;
-                            }while(count < idStudenti.size() && !flag);
-                        } else { // sezione di assegnamento dei progetti aperti
-                            ArrayList<String> idStudenti = progetto.getIdStudenti();
-                            int count = 0;
-                            boolean flag = false;
-
-                            do {
-                                if (idStudenti.get(count).equals(mAuth.getCurrentUser().getUid())) {
-                                    inProgressProject.add(progetto);
-                                    flag = true;
-                                }
-                                count++;
-                            }while(count < idStudenti.size() && !flag);
-                        }
-
-                        inProgressRecyclerView = viewHome.findViewById(R.id.inProgressRecyclerView);
-                        inProgressRecyclerView.setHasFixedSize(true);
-                        closedRecyclerView = viewHome.findViewById(R.id.closedRecyclerView);
-                        closedRecyclerView.setHasFixedSize(true);
-
-                        inProgressLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
-                        inProgressRecyclerView.setLayoutManager(inProgressLayoutManager);
-                        closedLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
-                        closedRecyclerView.setLayoutManager(closedLayoutManager);
-                        inProgressAdapter = new RecyclerViewAdapter(getActivity().getApplicationContext(), inProgressProject);
-                        inProgressRecyclerView.setAdapter(inProgressAdapter);
-                        closedAdapter = new RecyclerViewAdapter(getActivity().getApplicationContext(), closedProject);
-                        closedRecyclerView.setAdapter(closedAdapter);
+                        visualizzaProgettiDocente(esame, progetto);
                     }
-
-                    if (closedProject.isEmpty()) {
-                        RelativeLayout relativeLayout = viewHome.findViewById(R.id.closedContainer);
-                        relativeLayout.setVisibility(View.INVISIBLE);
-                    }
-                    if (inProgressProject.isEmpty()) {
-                        RelativeLayout relativeLayout = viewHome.findViewById(R.id.inProgressContainer);
-                        relativeLayout.setVisibility(View.INVISIBLE);
-                    }
+                    initProjectView();
                 }
             }
         });
+    }
 
-        return viewHome;
+    private void initProjectView() {
+        inProgressRecyclerView = viewHome.findViewById(R.id.inProgressRecyclerView);
+        inProgressRecyclerView.setHasFixedSize(true);
+        closedRecyclerView = viewHome.findViewById(R.id.closedRecyclerView);
+        closedRecyclerView.setHasFixedSize(true);
+
+        inProgressLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
+        inProgressRecyclerView.setLayoutManager(inProgressLayoutManager);
+        closedLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
+        closedRecyclerView.setLayoutManager(closedLayoutManager);
+        inProgressAdapter = new RecyclerViewAdapter(getActivity().getApplicationContext(), inProgressProject);
+        inProgressRecyclerView.setAdapter(inProgressAdapter);
+        closedAdapter = new RecyclerViewAdapter(getActivity().getApplicationContext(), closedProject);
+        closedRecyclerView.setAdapter(closedAdapter);
+
+        if (closedProject.isEmpty()) {
+            RelativeLayout relativeLayout = viewHome.findViewById(R.id.closedContainer);
+            relativeLayout.setVisibility(View.INVISIBLE);
+        }
+        if (inProgressProject.isEmpty()) {
+            RelativeLayout relativeLayout = viewHome.findViewById(R.id.inProgressContainer);
+            relativeLayout.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void visualizzaProgettiDocente(Esame exam, Progetto progetto) {
+        // sezione di assegnamento dei progetti chiusi
+        if(progetto.isClose()) {
+            ArrayList<String> idDocenti = exam.getIdDocenti();
+            int count = 0;
+            boolean flag = false;
+
+            do {
+                if (progetto.getCodiceEsame().equals(exam.getId())) {
+                    closedProject.add(progetto);
+                    flag = true;
+                }
+                count++;
+            }while(count < idDocenti.size() && !flag);
+
+        } else {
+            ArrayList<String> idDocenti = exam.getIdDocenti();
+            int count = 0;
+            boolean flag = false;
+
+            do {
+                if (progetto.getCodiceEsame().equals(exam.getId())) {
+                    inProgressProject.add(progetto);
+                    flag = true;
+                }
+                count++;
+            }while(count < idDocenti.size() && !flag);
+        }
+    }
+
+    private void visualizzaProgettiStudente(Progetto progetto) {
+        // sezione di assegnamento dei progetti chiusi
+        if(progetto.isClose()) {
+            ArrayList<String> idStudenti = progetto.getIdStudenti();
+            int count = 0;
+            boolean flag = false;
+
+            do {
+                if (idStudenti.get(count).equals(mAuth.getCurrentUser().getUid())) {
+                    closedProject.add(progetto);
+                    flag = true;
+                }
+                count++;
+            }while(count < idStudenti.size() && !flag);
+        } else { // sezione di assegnamento dei progetti aperti
+            ArrayList<String> idStudenti = progetto.getIdStudenti();
+            int count = 0;
+            boolean flag = false;
+
+            do {
+                if (idStudenti.get(count).equals(mAuth.getCurrentUser().getUid())) {
+                    inProgressProject.add(progetto);
+                    flag = true;
+                }
+                count++;
+            }while(count < idStudenti.size() && !flag);
+        }
     }
 
     @Override
